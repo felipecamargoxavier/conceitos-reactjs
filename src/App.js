770a4651431284cb0f7c146fee6b37fc0edd678a
Felipe Xavier <felipecamargoxav@hotmail.com>
2020-06-23 12:39:54 -0300

    implementacao dos conceitos de estado e imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  function handleAddRepository() {
+    api.post('/repositories', {
+      title: `teste repositorio ${Date.now()}`,
+      url: "teste.com.br",
+      techs: [1,2,3,4]
+    }).then(response => {
+      setRepositories([...repositories, response.data]);
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      const response = await api.delete(`/repositories/${id}`);
+
+      if(response.status === 204) {
+        var newRepositories = [...repositories];
+        const indexRepository = newRepositories.findIndex(respository => respository.id === id);
+        newRepositories.splice(indexRepository, 1);
+        setRepositories(newRepositories);
+      }
+
+    } catch(err) {
+      alert('Nao foi possivel deletar o repositorio');
+      console.log(err);
+    }
+  }
+
+  async function handleLikeRepository(id) {
+
+    try {
+      const response = await api.post(`/repositories/${id}/like`);
+
+      var newRepositories = [...repositories];
+      const indexRepository = newRepositories.findIndex(respository => respository.id === id);
+
+      newRepositories[indexRepository].likes = response.data.likes;
+      setRepositories(newRepositories);
+    } catch(err) {
+      alert('Nao foi possivel dar like no repositorio');
+      console.log(err);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button  onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+            <button onClick={() => handleLikeRepository(repository.id)}>
+              Like: {repository.likes}
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
